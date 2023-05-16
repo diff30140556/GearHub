7e84a675d31e754b0f342e31b855c9a3865c1046
Michaelx626 <118804354+Michaelx626@users.noreply.github.com>
2023-05-16 03:41:56 -0700

    fixed async await functions into a trycatch

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,36 +5,55 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
   Query: {
     findProducts: async (parent, { name }) => {
-      const product = await Product.find({ name });
-      return product;
+      try {
+        const product = await Product.find({ name });
+        
+        if(!product){
+          throw new Error('No Product Name Found!')
+        }
+
+        return product;
+
+      } catch (err) {
+        console.error(err);
+      }
     }
   },
 
   Mutation: {
     addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
+      try {
+        const user = await User.create(args);
+        const token = signToken(user);
+  
+        return { token, user };
 
-      return { token, user };
+      } catch (err) {
+        console.error(err);
+      }
     },
 
     login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
+      try {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError('No user found with this email address');
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const token = signToken(user);
+  
+        return { token, user };
 
-      if (!user) {
-        throw new AuthenticationError('No user found with this email address');
+      } catch (err) {
+        console.error(err);
       }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-      console.log({ user, token });
-
-      return { token, user };
     },
   },
 };
