089eda52bfcc9e86ce5f5dbb7c32669986c951d5
Michaelx626 <118804354+Michaelx626@users.noreply.github.com>
2023-05-15 22:28:51 -0700

    working on auth, resolver, and typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,41 +1,24 @@
-const { Thought } = require('../models');
+const { User } = require('../models');
 
 const resolvers = {
-  Query: {
-    thoughts: async () => {
-      return Thought.find();
-    },
-
-    thought: async (parent, { thoughtId }) => {
-      return Thought.findOne({ _id: thoughtId });
-    },
-  },
-
   Mutation: {
-    addThought: async (parent, { thoughtText, thoughtAuthor }) => {
-      return Thought.create({ thoughtText, thoughtAuthor });
-    },
-    addComment: async (parent, { thoughtId, commentText }) => {
-      return Thought.findOneAndUpdate(
-        { _id: thoughtId },
-        {
-          $addToSet: { comments: { commentText } },
-        },
-        {
-          new: true,
-          runValidators: true,
-        }
-      );
-    },
-    removeThought: async (parent, { thoughtId }) => {
-      return Thought.findOneAndDelete({ _id: thoughtId });
-    },
-    removeComment: async (parent, { thoughtId, commentId }) => {
-      return Thought.findOneAndUpdate(
-        { _id: thoughtId },
-        { $pull: { comments: { _id: commentId } } },
-        { new: true }
-      );
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      console.log({ user, token });
+
+      return { token, user };
     },
   },
 };
