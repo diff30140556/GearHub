4a0f8ce9d8a13d49c43baa56d6d8fef5867dc595
ShunWei Hu <87918961+diff30140556@users.noreply.github.com>
2023-05-18 02:05:12 -0700

    worked on landing page

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,58 +1,99 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
+import './style.css'
 
 function NavbarComp() {
     return (
         <>
-            <Navbar key='md' bg="light" expand='md' className="mb-3">
-                <Container fluid>
-                    <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
-                    <Navbar.Offcanvas
-                        id={`offcanvasNavbar-expand-$'md'`}
-                        aria-labelledby={`offcanvasNavbarLabel-expand-md`}
-                        placement="end"
-                    >
-                        <Offcanvas.Header closeButton>
-                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
-                                Offcanvas
-                            </Offcanvas.Title>
-                        </Offcanvas.Header>
-                        <Offcanvas.Body>
-                            <Nav className="justify-content-end flex-grow-1 pe-3">
-                                <Nav.Link href="#action1">Home</Nav.Link>
-                                <Nav.Link href="#action2">Link</Nav.Link>
-                                <NavDropdown
-                                    title="Dropdown"
-                                    id={`offcanvasNavbarDropdown-expand-md`}
-                                >
-                                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action4">
-                                        Another action
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action5">
-                                        Something else here
-                                    </NavDropdown.Item>
-                                </NavDropdown>
-                            </Nav>
-                            <Form className="d-flex">
-                                <Form.Control
-                                    type="search"
-                                    placeholder="Search"
-                                    className="me-2"
-                                    aria-label="Search"
-                                />
-                                <Button variant="outline-success">Search</Button>
-                            </Form>
-                        </Offcanvas.Body>
-                    </Navbar.Offcanvas>
+            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">GearHub</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-md-5">
+                            <NavDropdown title="Products" id="collasible-nav-dropdown">
+                                <div className='drop-down-section d-md-flex'>
+                                    <div className="category-box">
+                                        <h3 className='category-title'>Laptop</h3>
+                                        <ul className='category-list'>
+                                            <li>
+                                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                            </li>
+                                            <li>
+                                                <NavDropdown.Item href="#action/3.2">
+                                                    Another action
+                                                </NavDropdown.Item>
+                                            </li>
+                                            <li>
+                                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                            </li>
+                                            <li>
+                                                <NavDropdown.Item href="#action/3.4">
+                                                    Separated link
+                                                </NavDropdown.Item>
+
+                                            </li>
+
+                                        </ul>
+                                    </div>
+                                    <div className="category-box">
+                                        <h3 className='category-title'>Headphone</h3>
+                                        <ul className='category-list'>
+                                            <li>
+                                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                            </li>
+                                            <li>
+                                                <NavDropdown.Item href="#action/3.2">
+                                                    Another action
+                                                </NavDropdown.Item>
+                                            </li>
+                                            <li>
+                                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                            </li>
+                                            <li>
+                                                <NavDropdown.Item href="#action/3.4">
+                                                    Separated link
+                                                </NavDropdown.Item>
+
+                                            </li>
+
+                                        </ul>
+                                    </div>
+                                    <div className="category-box">
+                                        <h3 className='category-title'>Graphics Card</h3>
+                                        <ul className='category-list'>
+                                            <li>
+                                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                            </li>
+                                            <li>
+                                                <NavDropdown.Item href="#action/3.2">
+                                                    Another action
+                                                </NavDropdown.Item>
+                                            </li>
+                                            <li>
+                                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                            </li>
+                                            <li>
+                                                <NavDropdown.Item href="#action/3.4">
+                                                    Separated link
+                                                </NavDropdown.Item>
+
+                                            </li>
+
+                                        </ul>
+                                    </div>
+                                </div>
+                            </NavDropdown>
+                            <Nav.Link href="#features">About</Nav.Link>
+                            <Nav.Link href="#pricing">Support</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Cart</Nav.Link>
+                            <Nav.Link href="#memes">
+                                My Account
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+
                 </Container>
             </Navbar>
         </>
