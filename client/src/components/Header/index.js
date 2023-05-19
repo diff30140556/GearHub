5f46b03d16bc126384b214f3efe8eec8c19ed47f
ShunWei Hu <87918961+diff30140556@users.noreply.github.com>
2023-05-19 14:44:31 -0700

    pull code

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,84 +5,88 @@ function Header() {
     return (
         <header className='fixed-top'>
             <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-                {/* <Container> */}
+                <Container>
                     <Navbar.Brand href="#home">GearHub</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav navbarScroll" />
                     <Navbar.Collapse id="responsive-navbar-nav navbarScroll">
                         <Nav className="ms-md-5" navbarScroll>
                             <NavDropdown title="Products" id="collasible-nav-dropdown navbarScrollingDropdown">
-                                <div className='drop-down-section d-md-flex'>
-                                    <div className="category-box">
-                                        <h3 className='category-title'>Laptop</h3>
-                                        <ul className='category-list'>
-                                            <li>
-                                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                            </li>
-                                            <li>
-                                                <NavDropdown.Item href="#action/3.2">
-                                                    Another action
-                                                </NavDropdown.Item>
-                                            </li>
-                                            <li>
-                                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                            </li>
-                                            <li>
-                                                <NavDropdown.Item href="#action/3.4">
-                                                    Separated link
-                                                </NavDropdown.Item>
+                                <Container>
+                                    <div className='drop-down-section d-md-flex'>
+                                        <div className="category-box">
+                                            <h3 className='category-title'>Laptop</h3>
+                                            <ul className='category-list'>
+                                                <li>
+                                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                                </li>
+                                                <li>
+                                                    <NavDropdown.Item href="#action/3.2">
+                                                        Another action
+                                                    </NavDropdown.Item>
+                                                </li>
+                                                <li>
+                                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                                </li>
+                                                <li>
+                                                    <NavDropdown.Item href="#action/3.4">
+                                                        Separated link
+                                                    </NavDropdown.Item>
 
-                                            </li>
+                                                </li>
 
-                                        </ul>
+                                            </ul>
+                                        </div>
+                                        <div className="category-box">
+                                            <h3 className='category-title'>Headphone</h3>
+                                            <ul className='category-list'>
+                                                <li>
+                                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                                </li>
+                                                <li>
+                                                    <NavDropdown.Item href="#action/3.2">
+                                                        Another action
+                                                    </NavDropdown.Item>
+                                                </li>
+                                                <li>
+                                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                                </li>
+                                                <li>
+                                                    <NavDropdown.Item href="#action/3.4">
+                                                        Separated link
+                                                    </NavDropdown.Item>
+
+                                                </li>
+
+                                            </ul>
+                                        </div>
+                                        <div className="category-box">
+                                            <h3 className='category-title'>Graphics Card</h3>
+                                            <ul className='category-list'>
+                                                <li>
+                                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                                </li>
+                                                <li>
+                                                    <NavDropdown.Item href="#action/3.2">
+                                                        Another action
+                                                    </NavDropdown.Item>
+                                                </li>
+                                                <li>
+                                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                                </li>
+                                                <li>
+                                                    <NavDropdown.Item href="#action/3.4">
+                                                        Separated link
+                                                    </NavDropdown.Item>
+
+                                                </li>
+
+                                            </ul>
+                                        </div>
                                     </div>
-                                    <div className="category-box">
-                                        <h3 className='category-title'>Headphone</h3>
-                                        <ul className='category-list'>
-                                            <li>
-                                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                            </li>
-                                            <li>
-                                                <NavDropdown.Item href="#action/3.2">
-                                                    Another action
-                                                </NavDropdown.Item>
-                                            </li>
-                                            <li>
-                                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                            </li>
-                                            <li>
-                                                <NavDropdown.Item href="#action/3.4">
-                                                    Separated link
-                                                </NavDropdown.Item>
 
-                                            </li>
-
-                                        </ul>
-                                    </div>
-                                    <div className="category-box">
-                                        <h3 className='category-title'>Graphics Card</h3>
-                                        <ul className='category-list'>
-                                            <li>
-                                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                            </li>
-                                            <li>
-                                                <NavDropdown.Item href="#action/3.2">
-                                                    Another action
-                                                </NavDropdown.Item>
-                                            </li>
-                                            <li>
-                                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                            </li>
-                                            <li>
-                                                <NavDropdown.Item href="#action/3.4">
-                                                    Separated link
-                                                </NavDropdown.Item>
-
-                                            </li>
-
-                                        </ul>
-                                    </div>
-                                </div>
+                                </Container>
                             </NavDropdown>
+
                             <Nav.Link href="#features">About</Nav.Link>
                             <Nav.Link href="#pricing">Support</Nav.Link>
                         </Nav>
@@ -94,7 +98,7 @@ function Header() {
                         </Nav>
                     </Navbar.Collapse>
 
-                {/* </Container> */}
+                </Container>
             </Navbar>
         </header>
     )
