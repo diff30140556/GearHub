e8c2356d2e80afb175186a16481a2da4d3a65bac
Jabez <jabezli1990@gmail.com>
2023-05-18 19:17:27 -0700

    changes on mutation.js

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
@@ -11,4 +11,27 @@ export const LOGIN = gql`
   }
 `;
 
-export 
+export const ADD_ORDER = gql`
+  mutation addOrder($products: [ID]!) {
+    addOrder(products: $products) {
+      purchasedAt
+      products {
+        _id
+        name
+        price
+        isNew
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
