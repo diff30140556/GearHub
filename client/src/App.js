3aa1a6f72a0af394b14aa626eb503363796a32e8
ShunWei Hu <87918961+diff30140556@users.noreply.github.com>
2023-05-19 23:24:04 -0700

    push current code

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Header from './components/Header/index';
-import Footer from './components/Footer/index';
-import LandingPage from './pages/LandingPage/index';
-import ProductPage from './pages/ProductPage/index';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import Header from "./components/Header/index";
+import Footer from "./components/Footer/index";
+import ProductPage from "./pages/ProductPage/index";
+import LandingPage from "./pages/LandingPage/index";
+import LoginPage from "./pages/LoginPage/index";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-
       <Header />
-      {/* <LandingPage /> */}
-      <ProductPage />
+      {/* <LoginPage></LoginPage> */}
+      <LandingPage />
       <Footer />
     </div>
   );
--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -1,25 +1,38 @@
-import './style.css'
-import { ShoppingCartOutlined, AppstoreOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
-import { Row, Col } from 'react-bootstrap';
-import { useState } from 'react';
+import "./style.css";
+import { ShoppingCartOutlined, AppstoreOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import { Row, Col } from "react-bootstrap";
+import { useState } from "react";
 
 function LandingPage() {
-    const [size, setSize] = useState('large'); // default is 'middle'
+    const [size, setSize] = useState("large"); // default is 'middle'
     return (
-        <main >
+        <main>
             <div className="wrap">
-                <div className="productPage-section">
+                <div className="product-section">
                     <div className="product-box product-1 d-flex justify-content-center">
-                        <Row className='justify-content-center align-items-center flex-column-reverse flex-md-row'>
+                        <Row className="justify-content-center align-items-center flex-column-reverse flex-md-row">
                             <Col sm={12} md={5}>
                                 <div className="product-info">
-                                    <h2 className='text-white text-center fs-1'>The Westmire <br /><span>A56 Headset</span></h2>
+                                    <h2 className="text-white text-center fs-1">
+                                        The Westmire <br />
+                                        <span>A56 Headset</span>
+                                    </h2>
                                     <div className="product-btn d-flex justify-content-center">
-                                        <Button type="primary" shape="round" icon={<ShoppingCartOutlined className='btn-icon' />} size={size}>
+                                        <Button
+                                            type="primary"
+                                            shape="round"
+                                            icon={<ShoppingCartOutlined className="btn-icon" />}
+                                            size={size}
+                                        >
                                             Buy Now
                                         </Button>
-                                        <Button type="primary" shape="round" icon={<AppstoreOutlined className='btn-icon' />} size={size}>
+                                        <Button
+                                            type="primary"
+                                            shape="round"
+                                            icon={<AppstoreOutlined className="btn-icon" />}
+                                            size={size}
+                                        >
                                             Learn More
                                         </Button>
                                     </div>
@@ -33,15 +46,28 @@ function LandingPage() {
                         </Row>
                     </div>
                     <div className="product-box product-2 d-flex justify-content-center">
-                        <Row className='justify-content-center align-items-center flex-column-reverse flex-md-row'>
+                        <Row className="justify-content-center align-items-center flex-column-reverse flex-md-row">
                             <Col sm={12} md={5}>
                                 <div className="product-info">
-                                    <h2 className='text-white text-center fs-1'>The Westmire <br /><span>A56 Headset</span></h2>
+                                    <h2 className="text-white text-center fs-1">
+                                        The Westmire <br />
+                                        <span>A56 Headset</span>
+                                    </h2>
                                     <div className="product-btn d-flex justify-content-center">
-                                        <Button type="primary" shape="round" icon={<ShoppingCartOutlined className='btn-icon' />} size={size}>
+                                        <Button
+                                            type="primary"
+                                            shape="round"
+                                            icon={<ShoppingCartOutlined className="btn-icon" />}
+                                            size={size}
+                                        >
                                             Buy Now
                                         </Button>
-                                        <Button type="primary" shape="round" icon={<AppstoreOutlined className='btn-icon' />} size={size}>
+                                        <Button
+                                            type="primary"
+                                            shape="round"
+                                            icon={<AppstoreOutlined className="btn-icon" />}
+                                            size={size}
+                                        >
                                             Learn More
                                         </Button>
                                     </div>
@@ -55,15 +81,28 @@ function LandingPage() {
                         </Row>
                     </div>
                     <div className="product-box product-3 d-flex justify-content-center">
-                        <Row className='justify-content-center align-items-center flex-column-reverse flex-md-row'>
+                        <Row className="justify-content-center align-items-center flex-column-reverse flex-md-row">
                             <Col sm={12} md={5}>
                                 <div className="product-info">
-                                    <h2 className='text-white text-center fs-1'>The Westmire <br /><span>A56 Headset</span></h2>
+                                    <h2 className="text-white text-center fs-1">
+                                        The Westmire <br />
+                                        <span>A56 Headset</span>
+                                    </h2>
                                     <div className="product-btn d-flex justify-content-center">
-                                        <Button type="primary" shape="round" icon={<ShoppingCartOutlined className='btn-icon' />} size={size}>
+                                        <Button
+                                            type="primary"
+                                            shape="round"
+                                            icon={<ShoppingCartOutlined className="btn-icon" />}
+                                            size={size}
+                                        >
                                             Buy Now
                                         </Button>
-                                        <Button type="primary" shape="round" icon={<AppstoreOutlined className='btn-icon' />} size={size}>
+                                        <Button
+                                            type="primary"
+                                            shape="round"
+                                            icon={<AppstoreOutlined className="btn-icon" />}
+                                            size={size}
+                                        >
                                             Learn More
                                         </Button>
                                     </div>
@@ -77,15 +116,28 @@ function LandingPage() {
                         </Row>
                     </div>
                     <div className="product-box product-4 d-flex justify-content-center mb-0">
-                        <Row className='justify-content-center align-items-center flex-column-reverse flex-md-row'>
+                        <Row className="justify-content-center align-items-center flex-column-reverse flex-md-row">
                             <Col sm={12} md={5}>
                                 <div className="product-info">
-                                    <h2 className='text-white text-center fs-1'>The Westmire <br /><span>A56 Headset</span></h2>
+                                    <h2 className="text-white text-center fs-1">
+                                        The Westmire <br />
+                                        <span>A56 Headset</span>
+                                    </h2>
                                     <div className="product-btn d-flex justify-content-center">
-                                        <Button type="primary" shape="round" icon={<ShoppingCartOutlined className='btn-icon' />} size={size}>
+                                        <Button
+                                            type="primary"
+                                            shape="round"
+                                            icon={<ShoppingCartOutlined className="btn-icon" />}
+                                            size={size}
+                                        >
                                             Buy Now
                                         </Button>
-                                        <Button type="primary" shape="round" icon={<AppstoreOutlined className='btn-icon' />} size={size}>
+                                        <Button
+                                            type="primary"
+                                            shape="round"
+                                            icon={<AppstoreOutlined className="btn-icon" />}
+                                            size={size}
+                                        >
                                             Learn More
                                         </Button>
                                     </div>
@@ -101,6 +153,6 @@ function LandingPage() {
                 </div>
             </div>
         </main>
-    )
-};
+    );
+}
 export default LandingPage;
