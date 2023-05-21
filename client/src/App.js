3075bb0e214677ba06f3d46b70ed7eee320c71d8
Michaelx626 <118804354+Michaelx626@users.noreply.github.com>
2023-05-21 00:10:42 -0700

    login on the client side works now

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,56 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/index";
 import Footer from "./components/Footer/index";
-import ProductPage from "./pages/ProductPage/index";
+// import ProductPage from "./pages/ProductPage/index";
 import LandingPage from "./pages/LandingPage/index";
 import LoginPage from "./pages/LoginPage/index";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import { StoreProvider } from "./utils/GlobalState";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  console.log(headers, token)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-
-      {/* <LoginPage></LoginPage> */}
-      <LandingPage />
-
-      <Footer />
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="App">
+          {/* <StoreProvider> */}
+            <Header />
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+            </Routes>
+            <LandingPage />
+            <Footer />
+          {/* </StoreProvider> */}
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/pages/LoginPage/index.js
+++ client/src/pages/LoginPage/index.js
@@ -1,27 +1,61 @@
 import "./style.css";
-import { ShoppingCartOutlined, AppstoreOutlined } from "@ant-design/icons";
-import { Button } from "antd";
-import { Row, Col } from "react-bootstrap";
+// import { ShoppingCartOutlined, AppstoreOutlined } from "@ant-design/icons";
+// import { Button } from "antd";
+// import { Row, Col } from "react-bootstrap";
 import { useState } from "react";
+import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
+import { LOGIN } from '../../utils/mutation'
+import Auth from '../../utils/auth';
 
-function loginPage() {
+function LoginPage() {
+  const [formState, setFromState] = useState({ email: '', password: ''});
+  const [login, { error }] = useMutation(LOGIN);
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(formState.email, formState.password);
+      const response = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+
+      console.log(response)
+
+      const token = response.data.login.token;
+      Auth.login(token);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFromState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
   return (
     <main>
       <div className="wrap">
         <div className="login-box text-center">
           <h1>Login to GearHub</h1>
-          <form className="loginForm">
-            <label htmlFor="username">
-              <b>Username</b>
+          <form className="loginForm" onSubmit={handleFormSubmit}>
+            <label htmlFor="email">
+              <b>Email Address:</b>
             </label>
             <input
-              className="username shadow-sm"
+              className="email shadow-sm"
               type="text"
-              placeholder="Enter Username"
-              name="username"
+              placeholder="youremail@test.com"
+              name="email"
+              id="email"
               required
+              onChange={handleChange}
             />
-            <label htmlFor="password">
+            <label htmlFor="pwd">
               <b>Password</b>
             </label>
             <input
@@ -29,7 +63,9 @@ function loginPage() {
               type="password"
               placeholder="Enter Password"
               name="password"
+              id="pwd"
               required
+              onChange={handleChange}
             />
             <button className="shadow btn-Submit loginBtn" type="submit">
               Login
@@ -37,9 +73,9 @@ function loginPage() {
           </form>
           <p>
             Don't have an account?
-            <a className="signuplink" href="/signup">
+            <Link to="/signup">
               Sign up
-            </a>
+            </Link>
           </p>
         </div>
       </div>
@@ -47,4 +83,4 @@ function loginPage() {
   );
 }
 
-export default loginPage;
+export default LoginPage;
